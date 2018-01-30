b9b04a10e380871282f0ab41059b5a3c2aaf155a
Crabman <crabman@hypnoticowl.com>
2018-01-30 10:41:47 +0100

    Init

---
+++ js/game.js
@@ -0,0 +1,90 @@
+var Game = function Game()
+{
+    this.name = "Name";
+    this.init();
+}
+
+Game.prototype =
+{
+    init: function init()
+    {
+        this.container = $(document.createElement("div")).attr("id", "container");
+        this.profile = $(document.createElement("div")).attr("id", "profile");
+        this.newsfeed = $(document.createElement("div")).attr("id", "newsfeed");
+        
+        this.profile.append($(document.createElement("div")).attr("id", "profile-name").text(this.name)).append($(document.createElement("div")).attr("id", "profile-pic")).append($(document.createElement("div")).attr("id", "profile-feed-header").text("Shares")).append($(document.createElement("div")).attr("id", "profile-shares-container"));
+        
+        this.newsfeed.append($(document.createElement("div")).attr("id", "news-header")).append($(document.createElement("div")).attr("id", "news-container"));
+
+        $(this.container).append(this.profile);
+        $(this.container).append(this.newsfeed);
+        this.profile.hide();
+        
+        $("body").append($(document.createElement("div")).attr("id", "connectr-header")
+                        .append($(document.createElement("div")).attr("id", "connectr-header-container")
+                            .append($(document.createElement("div")).attr("id", "connectr-logo").text("Connectr"))
+                            .append($(document.createElement("div")).attr("id", "connectr-notifications"))));
+        $("body").append(this.container);
+        
+        this.createNewsItem("Ausländer sind doof!", "Behaupten wir jetzt einfach mal, weil die dauernd, also wirklich andauernd, so, naja, weißt schon, halt so manchmal, äh, also, ich mein, man kennt das ja...");
+    },
+    
+    createNewsItem: function createNewsItem(headline, msg, picURL, returnOnly)
+    {
+        var item = $(document.createElement("div")).addClass("news-item")
+                        .append($(document.createElement("div")).addClass("news-headline").text(headline))
+                        .append($(document.createElement("div")).addClass("news-body").text(msg));
+        
+        if (picURL)
+            item.append($(document.createElement("img")).addClass("news-pic").attr("src", picURL));
+        
+        item.append($(document.createElement("div")).addClass("news-response")
+                   .append($(document.createElement("div")).addClass("news-response-like").text("Like").click(this.respond.bind(this)))
+                   .append($(document.createElement("div")).addClass("news-response-dislike").text("Dislike").click(this.respond.bind(this)))
+                   .append($(document.createElement("div")).addClass("news-response-dismiss").text("Dismiss").click(this.respond.bind(this))));
+        
+        if (returnOnly)
+            return item;
+        else
+            this.newsfeed.find("#news-container").append(item);
+    },
+    
+    respond: function respond(e)
+    {
+        var item = $(e.currentTarget.parentElement.parentElement);
+        
+        if ($(e.currentTarget).hasClass("news-response-like"))
+        {
+            item.remove();
+            item.addClass("response-like");
+            item.find(".news-response").remove();
+            item.append($(document.createElement("div")).addClass("share-response"));
+            this.profile.find("#profile-shares-container").append(item);
+        }
+        else if ($(e.currentTarget).hasClass("news-response-dislike"))
+        {
+            item.remove();
+            item.addClass("response-dislike");
+            item.find(".news-response").remove();
+            item.append($(document.createElement("div")).addClass("share-response"));
+            this.profile.find("#profile-shares-container").append(item);
+        }
+        else
+            item.remove();
+        
+        if ($("#news-container").children().length == 0)
+            this.showProfile();
+    },
+    
+    showNewsfeed: function showNewsfeed()
+    {
+        this.profile.hide();
+        this.newsfeed.show();
+    },
+    
+    showProfile: function showProfile()
+    {
+        this.newsfeed.hide();
+        this.profile.show();
+    }
+};
