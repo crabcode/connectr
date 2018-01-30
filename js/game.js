ed351bfc88c9c2b3902f33893642397ed9e5a162
Crabman <crabman@hypnoticowl.com>
2018-01-30 13:28:11 +0100

    Tags etc

--- js/game.js
+++ js/game.js
@@ -1,6 +1,13 @@
 var Game = function Game()
 {
     this.name = "Name";
+    
+    this.interests = {
+        animals: 1,
+        sports: 1,
+        politics: 1
+    }
+    
     this.init();
 }
 
@@ -12,7 +19,7 @@ Game.prototype =
         this.profile = $(document.createElement("div")).attr("id", "profile");
         this.newsfeed = $(document.createElement("div")).attr("id", "newsfeed");
         
-        this.profile.append($(document.createElement("div")).attr("id", "profile-name").text(this.name)).append($(document.createElement("div")).attr("id", "profile-pic")).append($(document.createElement("div")).attr("id", "profile-feed-header").text("Shares")).append($(document.createElement("div")).attr("id", "profile-shares-container"));
+        this.profile.append($(document.createElement("div")).attr("id", "profile-pic")).append($(document.createElement("div")).attr("id", "profile-name").text(this.name)).append($(document.createElement("div")).attr("id", "profile-feed-header").text("Shares")).append($(document.createElement("div")).attr("id", "profile-shares-container"));
         
         this.newsfeed.append($(document.createElement("div")).attr("id", "news-header").text("Newsfeed")).append($(document.createElement("div")).attr("id", "news-container"));
 
@@ -23,25 +30,40 @@ Game.prototype =
         $("body").append($(document.createElement("div")).attr("id", "connectr-header")
                         .append($(document.createElement("div")).attr("id", "connectr-header-container")
                             .append($(document.createElement("div")).attr("id", "connectr-logo").text("Connectr"))
-                            .append($(document.createElement("div")).attr("id", "connectr-notifications"))));
+                            .append($(document.createElement("div")).attr("id", "connectr-profile").addClass("alert").click(this.showProfile.bind(this)))
+                            .append($(document.createElement("div")).attr("id", "connectr-feed").addClass("alert").click(this.showNewsfeed.bind(this)))));
         $("body").append(this.container);
         
-        this.createNewsItem("Ausländer sind doof!", "Behaupten wir jetzt einfach mal, weil die dauernd, also wirklich andauernd, so, naja, weißt schon, halt so manchmal, äh, also, ich mein, man kennt das ja...");
+        this.createNewsItem("Ausländer sind doof!", "Behaupten wir jetzt einfach mal, weil die dauernd, also wirklich andauernd, so, naja, weißt schon, halt so manchmal, äh, also, ich mein, man kennt das ja...", ["politics"], 1);
         
-        this.createNewsItem("Nee, du bist doof!", "Das ist ja jetzt auch nicht ganz richtig, find ich.");
+        this.createNewsItem("Nee, du bist doof!", "Das ist ja jetzt auch nicht ganz richtig, find ich.", ["politics"], -1);
     },
     
-    createNewsItem: function createNewsItem(headline, msg, picURL, returnOnly)
+    createNewsItem: function createNewsItem(headline, msg, tags, agenda, picURL, returnOnly)
     {
         var item = $(document.createElement("div")).addClass("news-item-wrapper")
                         .append($(document.createElement("div")).addClass("news-item")
                             .append($(document.createElement("div")).addClass("news-headline").text(headline))
                             .append($(document.createElement("div")).addClass("news-body").text(msg)));
+        switch(tags)
+        {
+            case "animals":
+                item.attr("tag", "animal");
+                break;
+            case "sports":
+                item.attr("tag", "sports");
+                break;
+            case "politics":
+                item.attr("tag", "politics");
+                break;
+        }
+        
+        item.attr("agenda", agenda);
         
         if (picURL)
             item.append($(document.createElement("img")).addClass("news-pic").attr("src", picURL));
         
-        item.append($(document.createElement("div")).addClass("news-response")
+        item.find(".news-item").append($(document.createElement("div")).addClass("news-response")
                    .append($(document.createElement("div")).addClass("news-response-like").text("Like").click(this.respond.bind(this)))
                    .append($(document.createElement("div")).addClass("news-response-dislike").text("Dislike").click(this.respond.bind(this)))
                    .append($(document.createElement("div")).addClass("news-response-dismiss").text("Dismiss").click(this.respond.bind(this))));
@@ -63,6 +85,14 @@ Game.prototype =
             item.find(".news-response").remove();
             item.append($(document.createElement("div")).addClass("share-response"));
             this.profile.find("#profile-shares-container").append(item);
+            
+            if(item.hasClass("animals"))
+                this.parentElement.interests.animals++;
+            if(item.hasClass("sports"))
+                this.parentElement.interests.sports++;
+            if(item.hasClass("politics"))
+                this.parentElement.interests.politics++;
+            
         }
         else if ($(e.currentTarget).hasClass("news-response-dislike"))
         {
