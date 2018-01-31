e6bdc7030e82d60430359b3440527f582faa85da
Crabman <crabman@hypnoticowl.com>
2018-01-31 11:56:42 +0100

    Content filled in

--- js/content.js
+++ js/content.js
@@ -6,28 +6,28 @@ var createContent = function(game)
     [//Day1
         this.game.createNewsItem(
         "Neues Nintendo Spiel angekündigt",
-        "",
+        "Nintendo hat in einer Videopräsentation im Rahmen der Technikmesse eine Reihe neuer Titel angekündigt, unter anderem dem heiß ersehnten...",
         ["gaming", "nintendo", "newgame"],
         0,
         "assets/nintendo.jpg",true),
 
         this.game.createNewsItem(
         "Kickers im Viertelfinale!",
-        "",
+        "Dass man sowas noch erleben darf! Haben es die Jungs mal wieder geschafft! Der Ball wurde mehrfach getreten und ist manchmal sogar ins Netz gegangen. Unglaublich!",
         ["soccer", "kickers"],
         0,
         "assets/kickers.jpeg",true),
 
         this.game.createNewsItem(
-        "Regierung arbeitet an Gesetztesänderung",
-        "",
+        "Regierung arbeitet an Gesetzesänderung",
+        "Bundestag, 2018. Die Stimmung ist gespannt. Herr Seehofer schwitzt schon wieder. Die Diskussion ist in vollem Gange. Worauf...",
         ["politics"],
         0,
         "assets/bundestag.jpg",true),
 
         this.game.createNewsItem(
         "Das tragen die Stars!",
-        "",
+        "Was haben Kim Kardashian, Emma Watson und Jessica Chastain gemeinsam? Finden Sie es heraus, in unserer großen Reportage des...",
         ["celebrities"],
         1,
         "assets/roterteppich.jpg",true),
@@ -42,14 +42,14 @@ var createContent = function(game)
     [//Day2
         this.game.createNewsItem(
         "Nintendos Reveal-Trailer",
-        "",
+        "Kaut ihr auch schon seit Tagen nervös auf euren Fingernägeln und wartet sehnsüchtig auf den neuen Nintendo-Trailer? Euer Warten hat nun ein Ende!",
         ["gaming", "nintendo", "newgame"],
         0,
         "assets/nintendo.jpg",true),
 
         this.game.createNewsItem(
-        "Unsere E3-Vorhersagen",
-        "",
+        "E3-Geheimnisse geleaked!",
+        "Was wird es dieses Jahr auf der E3 zu sehen geben? Wir haben mit anonymen Quellen gesprochen und den exklusiven Blick in die Geheimnisse der Spieleindustrie!",
         ["gaming", "e3", "journalism"],
         0,
         "assets/e3.jpg",true),
@@ -70,7 +70,7 @@ var createContent = function(game)
 
         this.game.createNewsItem(
         "Techniktrends 2018",
-        "",
+        "Was ist in? Was ist out? Erfahre alles, was du für dieses Jahr wissen musst in unserem Techniktrends-Überblick!",
         ["tech"],
         0,
         "assets/tech.jpg",true)
@@ -78,14 +78,14 @@ var createContent = function(game)
     [//Day3
         this.game.createNewsItem(
         "7/10 für Nintendos neues Spiel",
-        "",
+        "Nintendo hat mal wieder ein Spiel produziert, das trotz kleiner Mängel überzeugen kann und viele Stunden Spaß bereithält.",
         ["gaming", "nintendo", "newgame"],
         -2,
         "assets/7-10.jpg",true),
 
         this.game.createNewsItem(
         "Nintendos neues Meisterwerk",
-        "",
+        "Es ist kaum zu glauben: Nintendo setzt erneut neue Maßstäbe für Qualität, Spielspaß und emotionale Dichte.",
         ["gaming", "nintendo", "newgame"],
         2,
         "assets/10-10.jpg",true),
@@ -105,8 +105,8 @@ var createContent = function(game)
         "assets/cat-hardmode.jpg",true),
 
         this.game.createNewsItem(
-        "Finanztipps 2018",
-        "So investieren die Profis!",
+        "Finanztipps 2018 - So investieren die Profis!",
+        "Taschengeld aufbessern oder Haus finanzieren? Mit diesen Tipps können Sie ihr Erspartes zu einem Vermögen verwandeln!",
         ["finance"],
         1,
         "assets/finance.jpg",true)
@@ -114,21 +114,21 @@ var createContent = function(game)
     [//Day3
         this.game.createNewsItem(
         "Game of the Year von Nintendo",
-        "",
+        "2018 hat gerade erst begonnen und schon haben wir den ersten Game Of The Year-Kandidaten!",
         ["gaming", "nintendo", "newgame"],
         2,
         "assets/goty.jpg",true),
 
         this.game.createNewsItem(
         "Hotfix für neues Nintendo-Spiel",
-        "",
+        "Kein Produkt ist perfekt. Jedoch bemüht sich Nintendo schneller als andere, auch die kleinen Mängel zu beheben. Hier die wichtigsten Änderungen:",
         ["gaming", "nintendo", "newgame"],
         0,
         "assets/nintendo.jpg",true),
 
         this.game.createNewsItem(
         "Spielepresse korrupt?",
-        "",
+        "Wie bekommt Nintendos Meisterwerk eine vernichtende 7/10, während generische Shooter 10/10 absahnen? Wir untersuchen, wie Journalisten sich von ihren Gefühlen und anderen Anreizen lenken lassen.",
         ["gaming", "newgame", "journalism"],
         2,
         "assets/bias.jpg",true),
@@ -145,26 +145,26 @@ var createContent = function(game)
         "",
         ["cats"],
         0,
-        null,true)
+        "assets/cat-disapproves.jpg",true)
     ],
     [//Day4
         this.game.createNewsItem(
-        "Sex für Reviews?",
-        "Spielejournalistin erwischt!",
+        "Sex für Reviews? Skandal aufgedeckt!",
+        "Wer hat sich nicht den Kopf gekratzt, als die Presse sich vor Lob für ein mittelmäßiges Text- \"spiel\" nicht retten konnte? Nun ist klar warum:",
         ["gaming", "journalism", "newgame"],
         5,
         "assets/busted.png",true),
 
         this.game.createNewsItem(
-        "Gamers are Dead!",
-        "",
+        "Gamers Are Dead",
+        "Was bedeutet \"Gamer\" heutzutage überhaupt noch? Ist es an der Zeit, dass wir uns von der Identität \"Gamer\" verabschieden?",
         ["gaming", "journalism"],
         -2,
         "assets/gamers-dead.jpg",true),
 
         this.game.createNewsItem(
         "DLC für Nintendos neuestes Spiel",
-        "",
+        "Fans sind entzückt: Nintendo kündigt neue Inhalte für ihren neuesten Titel an, der Million in seinen Bann gezogen hat",
         ["gaming", "newgame", "nintendo"],
         0,
         "assets/Season-Pass.png",true),
@@ -174,33 +174,33 @@ var createContent = function(game)
         "",
         ["cats", "meme"],
         1,
-        null,true),
+        "assets/cat-opinion.jpg",true),
 
         this.game.createNewsItem(
         "",
         "",
         ["cats", "meme"],
         0,
-        null,true)
+        "assets/cat-no.jpg",true)
     ],
     [//Day5
         this.game.createNewsItem(
         "Petition gegen unfaire Bewertung",
-        "",
+        "Wir haben es satt, wie sich vermeintliche \"Journalisten\" gegen Entwickler wie Nintendo verschwören, weil es gerade trendy ist, konträr zu sein!",
         ["gaming", "newgame", "journalism"],
         3,
         "assets/stop.jpg",true),
 
         this.game.createNewsItem(
         "Ethik des Spielejournalismus",
-        "",
+        "Die Spielepresse hat ein Korruptionsproblem. Spiele werden Geisel genommen und bei nicht ausreichenden \"Gefallen\" in den Reviews vernichtet.",
         ["gaming", "journalism"],
         4,
         "assets/gg.jpg",true),
 
         this.game.createNewsItem(
-        "Feministen gegen Gamer",
-        "Wie Frauen uns unsere Spiele wegnehmen wollen",
+        "Feminists vs. Gamers",
+        "Keine Gewalt, kein Sex, kein Spaß. Wenn es nach diesen Frauen ginge, könnten wir bald nur noch Walking-Simulatoren spielen.",
         ["gaming", "journalism"],
         5,
         "assets/feminazi.jpg",true),
@@ -210,11 +210,11 @@ var createContent = function(game)
         "",
         ["cats", "meme", "gaming"],
         3,
-        null,true),
+        "assets/cat-feminism.jpg",true),
 
         this.game.createNewsItem(
         "Ban Gamer Cat?",
-        "",
+        "Ich wusste nicht, dass Katzen sexistisch sein können! Aber scheinbar wollen diese Feminazis uns nun auch noch die Memes wegnehmen!",
         ["cats", "meme", "gaming", "journalism"],
         -1,
         "assets/banned.jpg",true)
@@ -222,14 +222,14 @@ var createContent = function(game)
     [//Day6
         this.game.createNewsItem(
         "Zensur in der Spielepresse",
-        "",
+        "Gelöschte oder gar ganz abgeschaltete Kommentare und vieles mehr: Unsere Meinungsfreiheit wird untergraben und attackiert!",
         ["gaming", "journalism", "mensrights"],
         3,
         "assets/censorship-2.jpg",true),
 
         this.game.createNewsItem(
-        "PC-Culture in Spielen",
-        "Wie PC-Culture unsere Spiele ruiniert.",
+        "Wie PC-Culture unsere Spiele ruiniert",
+        "Es ist überall: Heulende Pflänzchen, die alle ihre Extrawurst haben wollen. Aber dürfen wir unsere Spiele noch genießen? Nein, das würde ja ihre Gefühle verletzen!",
         ["gaming", "journalism"],
         4,
         "assets/censorship.jpg",true),
@@ -239,11 +239,11 @@ var createContent = function(game)
         "",
         ["gaming", "journalism", "cats", "meme"],
         5,
-        null,true),
+        "assets/cat-ethics.jpg",true),
 
         this.game.createNewsItem(
-        "Misandry",
-        "",
+        "Misandrie - Ein wachsendes Problem",
+        "Darf man heutzutage überhaupt noch Mann sein? Scheinbar ist alles an uns falsch und anstößig. Wer hilft uns gegen wachsenden Männerhass?",
         ["gaming", "journalism", "mensrights"],
         5,
         "assets/mra.jpg",true),
@@ -253,33 +253,33 @@ var createContent = function(game)
         "",
         ["cats", "meme", "gaming"],
         4,
-        null,true)
+        "assets/cat-hypocrit.jpg",true)
     ],
-    [//Day6
+    [//Day7
         this.game.createNewsItem(
-        "Games die deine Freundin hassen wird",
-        "",
+        "Feministen hassen diese Spiele",
+        "Mit Spiel #5 wirst du diese Mimöschen zum weinen bringen!",
         ["gaming", "mensrights"],
         5,
         "assets/feminists.jpg",true),
 
         this.game.createNewsItem(
-        "Studie zeigt: Männer am meisten unterdrückte Gruppierung",
-        "",
+        "Studie zeigt: Männer am meisten unterdrückte Gruppe",
+        "Es ist zum Nationalsport geworden, Männer zu diskriminieren. Wann immer wir diese Ungerechtigkeit dann ankreiden, werden wir verspottet und abgewiesen. Genug!",
         ["gaming", "journalism", "mensrights"],
         5,
         "assets/oppression.jpg",true),
 
         this.game.createNewsItem(
-        "Age of the Snowflake",
-        "",
+        "The Age of the Snowflake",
+        "\"Buhuhu, ich bin ja so getriggered!\" So schallt es dieser Tage überall und wir sollen uns dem beugen? Niemals!",
         ["journalism", "mensrights", "politics"],
         4,
         "assets/snowflake.jpg",true),
 
         this.game.createNewsItem(
         "Präsidentschaftskandidat sagt was wir alle denken",
-        "",
+        "Ein Funken Hoffnung erglimmt im Dunkel: Wer hätte gedacht, dass es heutzutage noch möglich ist, sich dem feministischen Apparat zu wider- setzen und Wahrheit stolz auszusprechen!",
         ["mensrights", "politics"],
         5,
         "assets/trump.jpg",true),
@@ -289,7 +289,7 @@ var createContent = function(game)
         "",
         ["cats", "meme", "gaming"],
         0,
-        null,true)
+        "assets/cat-censorship.jpg",true)
     ]
 ];
 } 
